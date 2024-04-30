0a4ba6f5bf8f76609e17e5b58dc7165472aeb746
Juan Carlos Domínguez <jdominguez@priceshoes.com>
2024-04-29 20:31:03 -0600

    Mostrando productos en ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,11 +3,22 @@ import { createContext, useState } from 'react';
 export const ShopingCartContext = createContext();
 
 export const ShoppingCartProvider= ({children}) => {
+    // Shopping Cart (Increment quantity)
     const [count, setCount] = useState(0);
+
+    // Shopping Cart (Background Color)
     const [colorfill, setColorfill] = useState("white");
+
+    // Product Detail (Open / Close)
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
     const openProductDetail = () => setIsProductDetailOpen(true);
     const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Product Detail (Show Product)
+    const [productToShow, setProductToShow] = useState({});
+
+    // Shopping Cart (Add Products to Cart)
+    const [cartProducts, setCartProducts] = useState([]);
 
     return (
         <ShopingCartContext.Provider value={{
@@ -18,7 +29,11 @@ export const ShoppingCartProvider= ({children}) => {
             isProductDetailOpen,
             setIsProductDetailOpen,
             openProductDetail,
-            closeProductDetail
+            closeProductDetail,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts
         }}>
             {children}
         </ShopingCartContext.Provider>
