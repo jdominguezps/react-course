e3975ee0cb2f1ca5be6d66a9a197b1a97713810f
Juan Carlos Domínguez <jdominguez@priceshoes.com>
2024-05-06 21:17:09 -0600

    Eliminar productos del carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -7,6 +7,12 @@ import './CheckoutSideMenu.css';
 const CheckoutSideMenu = () => {
     const context = useContext(ShopingCartContext);
 
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id);
+        context.setCartProducts(filteredProducts);
+        context.setCount(context.count - 1);
+    };
+
     return (
         <aside
             className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -23,9 +29,11 @@ const CheckoutSideMenu = () => {
             {context.cartProducts.map(product => (
                 <OrderCard
                     key={product.id}
+                    id={product.id}
                     title={product.title}
                     imageUrl={product.images}
                     price={product.price}
+                    handleDelete={handleDelete}
                 />
               ))
             }
