e904cd645f9b9d50c82c191669f70a6eef9aab11
Juan Carlos Domínguez <jdominguez@priceshoes.com>
2024-05-29 20:21:37 -0600

    Página de MyOrder: órden individual

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -7,6 +7,9 @@ import Layout from "../../Components/Layout";
 
 function MyOrder() {
   const context = useContext(ShopingCartContext);
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+  if (index === 'last') index = context.order?.length - 1;
 
   return (
     <Layout>
@@ -17,7 +20,7 @@ function MyOrder() {
         <h1>My Order</h1>
       </div>
       <div className='flex flex-col w-80'>
-            {context.order?.slice(-1)[0].products.map(product => (
+            {context.order?.[index]?.products.map(product => (
                 <OrderCard
                     key={product.id}
                     id={product.id}
